6b46b6eb39712f137009f0926243d7c5c389938f
Porumb Mihai-Cristian <cristianporumb3@gmail.com>
2023-12-13 10:38:09 +0200

    Initial commit

---
+++ src/main/java/com/hcl/betproblem/betproblem/BetproblemApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.betproblem.betproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetproblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/betproblem/betproblem/BetproblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.betproblem.betproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetproblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
